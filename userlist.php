a158c9b3806bb1931eb8792e4b332739ade15f6d
Zanux <daulton@ffxi.email>
2025-12-19 19:14:01 -0700

    Update userlist.php
    Better layout fix

--- userlist.php
+++ userlist.php
@@ -10,14 +10,14 @@ require ('corefiles/core.php');
 printheader();
 
 print "<table $themesettings[tableAttributes] class=\"table\">
-	<tr><th $themesettings[thRegularAttributes] class=\"thRegular\">username</th><th $themesettings[thRegularAttributes] class=\"thRegular\" width=200>date registered</th><th $themesettings[thRegularAttributes] class=\"thRegular\" width=80># posts</th></tr>";
+	<tr><th $themesettings[thRegularAttributes] class=\"thRegular\" align=left>Name</th><th $themesettings[thRegularAttributes] class=\"thRegular\" width=200>Joined</th><th $themesettings[thRegularAttributes] class=\"thRegular\" width=80>Posts</th></tr>";
 
 $users_query=mysqli_query($sql, "SELECT `id`, `regdate`, (SELECT COUNT(*) FROM `posts` WHERE `userid` = `users`.`id`) AS `numposts` FROM `users` ORDER BY `numposts` DESC");
 
 while($users_data=mysqli_fetch_array($users_query))
 {
 
-	print "<tr><td $themesettings[tdStyle1Attributes] class=\"tdStyle1\">".getusername($users_data['id'])."</td><td $themesettings[tdStyle1Attributes] class=\"tdStyle1\">".date("Y-m-d H:i:s", $users_data['regdate'])."</td><td $themesettings[tdStyle1Attributes] class=\"tdStyle1\">$users_data[numposts]</td></tr>";
+	print "<tr><td $themesettings[tdStyle1Attributes] class=\"tdStyle1\">".getusername($users_data['id'])."</td><td $themesettings[tdStyle1Attributes] class=\"tdStyle1\" align=center>".date("Y-m-d H:i:s", $users_data['regdate'])."</td><td $themesettings[tdStyle1Attributes] class=\"tdStyle1\" align=center>$users_data[numposts]</td></tr>";
 }
 
 
