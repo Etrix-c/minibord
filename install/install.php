79f87c4b34a5c1f9a5b1c29a978e738302e36b26
Zanux <daulton@ffxi.email>
2025-12-19 20:16:05 -0700

    Update install.php
    Quick and dirty install file

--- install/install.php
+++ install/install.php
@@ -1,7 +1,28 @@
 <?php
 /********************************
 * FFXIBord installation script  *
-* Copyright (C) 2026 FFXI.EMAIL *
 *********************************/
 
+$servername = "localhost";
+$username = "root"; // Use a user with sufficient permissions
+$password = "your_mysql_password";
+// Connect without specifying a database name initially
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Read the SQL file content
+$sql_queries = file_get_contents("install.sql");
+
+// Execute multiple queries
+if (mysqli_multi_query($conn, $sql_queries)) {
+    echo "FFXIBord Installed! Please delete the install directory.!";
+} else {
+    echo "Error installing database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
