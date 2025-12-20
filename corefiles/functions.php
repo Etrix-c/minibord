ed298db6f03f9d702d152d8442774a782fb5e756
Zanux <daulton@ffxi.email>
2025-12-19 21:01:49 -0700

    Update functions.php

--- corefiles/functions.php
+++ corefiles/functions.php
@@ -24,7 +24,7 @@ function printfooter()
 
 //echo "<p>Query executed in ".$exec_time_row[1].' seconds';
 	
-	print "</div><div class=\"footer\">Powered By <a herf=https://github.com/Etrix-c/FFXIBord>FFXIBord [v:0.1B]</a> | A fork of minibord <a href=https://github.com/NinCollin/minibord>minibord</a><br>PHP execution time: $rendertime</div>";
+	print "</div><div class=\"footer\">Powered By <a href=https://github.com/Etrix-c/FFXIBord>FFXIBord [v:0.1B]</a> | A fork of minibord <a href=https://github.com/NinCollin/minibord>minibord</a><br><br>PHP execution time: $rendertime</div>";
 }
 
 
